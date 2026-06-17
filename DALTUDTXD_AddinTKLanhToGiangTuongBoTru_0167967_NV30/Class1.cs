00dda395e16bf768703d5c41fcfca685c644550d
hoangvancuong15 <0167967@st.huce.edu.vn>
2026-06-18 04:09:55 +0700

    Add project files.

---
+++ DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30/Class1.cs
@@ -0,0 +1,8 @@
+
+namespace DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30
+{
+    public class Class1
+    {
+    }
+
+}
---
+++ DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30/Views/DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30View.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30.Views
+{
+    /// <summary>
+    /// Interaction logic for DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30View.xaml
+    /// </summary>
+    public partial class DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30View : Window
+    {
+        public DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30View()
+        {
+            InitializeComponent();
+        }
+    }
+}

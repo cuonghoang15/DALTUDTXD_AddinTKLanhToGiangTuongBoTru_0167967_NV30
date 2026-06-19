a867734b226ee5b66a0c73981d35bab6ae83e1d7
hoangvancuong15 <0167967@st.huce.edu.vn>
2026-06-19 16:18:33 +0700

    cuonghoang15/feature/CuaItem

--- DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30/Models/CuaItem.cs
+++ DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30/Models/CuaItem.cs
@@ -5,5 +5,10 @@
     /// </summary>
     public class CuaItem
     {
+        public string TenCua { get; set; } = string.Empty;
+        /// <summary>Chiều rộng cửa (m)</summary>
+        public double RongCua { get; set; }
+        /// <summary>Chiều dài lanh tô = rộng cửa + 2 × chiều dài gối (m)</summary>
+        public double DaiLanhTo { get; set; }
     }
 }

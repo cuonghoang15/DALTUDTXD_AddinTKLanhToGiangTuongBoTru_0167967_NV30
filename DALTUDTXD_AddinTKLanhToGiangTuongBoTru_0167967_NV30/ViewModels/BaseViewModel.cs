191d1956a9c5df2cb91e658d0fc128f32ca63dc8
hoangvancuong15 <0167967@st.huce.edu.vn>
2026-06-19 17:39:59 +0700

    cuonghoang15/feature/ViewModels/LanhToviewModels/Thông số đầu vào

--- DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30/ViewModels/BaseViewModel.cs
+++ DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30/ViewModels/BaseViewModel.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30.ViewModels
 {
-    public class BaseViewModel
+    /// <summary>
+    /// Lớp nền tảng cài đặt INotifyPropertyChanged cho tất cả ViewModel
+    /// </summary>
+    public abstract class BaseViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? name = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(name);
+            return true;
+        }
     }
 }
--- DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30/ViewModels/LanhToViewModel.cs
+++ DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30/ViewModels/LanhToViewModel.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30.ViewModels
 {
-    public class LanhToViewModel
+    public class LanhToViewModel : BaseViewModel
     {
+        // ── Thông số đầu vào ─────────────────────────────────────────────────
+
+        private double _chieuDaiGoi = 0.15;
+        /// <summary>Chiều dài gối lanh tô mỗi bên (m)</summary>
+        public double ChieuDaiGoi
+        {
+            get => _chieuDaiGoi;
+            set => SetField(ref _chieuDaiGoi, value);
+        }
+
+        private double _chieuCaoLanhTo = 0.2;
+        public double ChieuCaoLanhTo
+        {
+            get => _chieuCaoLanhTo;
+            set => SetField(ref _chieuCaoLanhTo, value);
+        }
+
+        private double _chieuRongTietDien = 0.2;
+        public double ChieuRongTietDien
+        {
+            get => _chieuRongTietDien;
+            set => SetField(ref _chieuRongTietDien, value);
+        }
     }
 }

203571337e4335000071a434653d1af32e06fdda
Truong258004 <truongle2582004@gmail.com>
2026-06-19 15:49:47 +0700

    thongso
    Truong258004/feature/Thongsodauvao

--- DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30/Models/TuongBoTruItem.cs
+++ DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30/Models/TuongBoTruItem.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30.Models
 {
-    class TuongBoTruItem
+   public class TuongBoTruItem
     {
+        public string TenTuong { get; set; } = string.Empty;
+        public double ChieuDai { get; set; }
+        public double ChieuCao { get; set; }
+        public double SoBoTru { get; set; }
+
     }
 }

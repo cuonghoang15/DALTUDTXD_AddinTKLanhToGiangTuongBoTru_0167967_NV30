5259dfd7eecbab9bc9236601b4e1a5cf1ceb1016
Truong258004 <truongle2582004@gmail.com>
2026-06-19 16:09:47 +0700

    Update BoTruViewModel.cs
    Truong258004/feature/ViewModels/Laythongso

--- DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30/ViewModels/BoTruViewModel.cs
+++ DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30/ViewModels/BoTruViewModel.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30.Models;
 
 namespace DALTUDTXD_AddinTKLanhToGiangTuongBoTru_0167967_NV30.ViewModels
 {
-    class BoTruViewModel
+     public class BoTruViewModel : BaseViewModel
     {
+        // ── Thông số đầu vào ─────────────────────────────────────────────────
+
+        private double _chieuDaiTuong = 5.0;
+        public double ChieuDaiTuong
+        {
+            get => _chieuDaiTuong;
+            set => SetField(ref _chieuDaiTuong, value);
+        }
+        private double _chieuCaoTuong = 3.6;
+        public double ChieuCaoTuong
+        {
+            get => _chieuCaoTuong;
+            set => SetField(ref _chieuCaoTuong, value);
+        }
+
+        private double _khoangCachBoTruToiDa = 4.0;
+        public double KhoangCachBoTruToiDa
+        {
+            get => _khoangCachBoTruToiDa;
+            set => SetField(ref _khoangCachBoTruToiDa, value);
+        }
     }
 }
